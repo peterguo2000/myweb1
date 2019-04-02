0a6c65af22f960f68458fc0a3c90bfd61cab9c19
peterguo2000 <peter.k.guo@gmail.com>
2019-04-02 17:16:14 +1100

    Update app.component.ts

--- dbwatcher-ui/src/app/app.component.ts
+++ dbwatcher-ui/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { APIService } from './services/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'dbwatcher-ui';
